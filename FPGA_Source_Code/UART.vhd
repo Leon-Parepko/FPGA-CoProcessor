df5a3379c2810296f92643a6cb51c57e1bfef1d5
Leon-Parepko <l.parepko@innopolis.university>
2021-11-13 23:22:56 +0600

    added UART reciver/transmiter(with loop) and block scheme

--- FPGA_Source_Code/UART.vhd
+++ FPGA_Source_Code/UART.vhd
@@ -7,10 +7,12 @@ use IEEE.STD_Logic_1164.ALL;
 -- Define Clock as input, rx as input, tx as output and LED_rx as Output
 
 entity UART is
-	Port(	clk:		in STD_LOGIC;
-			rx:		in STD_LOGIC;
-			tx:		out STD_LOGIC;
-			LED_rx: 	out STD_LOGIC);
+	Port(	clk:				in STD_LOGIC;
+			process_in:		in  STD_LOGIC;
+			rx:				in STD_LOGIC;
+			process_out:	out  STD_LOGIC;
+			tx:				out STD_LOGIC;
+			LED_rx: 			out STD_LOGIC);
 	end UART;
 
 
@@ -22,13 +24,20 @@ begin
 -- Instructions are executed sequencially with the process
 	process(clk)
 	
-	variable counter:			integer range 0 to 50000 :=0;								-- Counter used for reading rx pin
-	variable counter_2:		integer range 0 to 50000 :=0;								-- Counter used for reading tx pin
-	variable tx_flag:			std_LOGIC :='0';												-- Flag to start transmitting data on tx pin
-	variable UART_buffer: 	STD_LOGIC_VECTOR (9 DOWNTO 0) :="0000000000";		-- Array to store incoming data
+	variable counter:				integer range 0 to 50000 :=0;								-- Counter used for reading rx pin
+	variable counter_2:			integer range 0 to 50000 :=0;								-- Counter used for reading tx pin
+	
+	variable process_counter:		integer range 0 to 50000 :=0;				
+	variable process_counter_2:	integer range 0 to 50000 :=0;	
+	
+	variable tx_flag:				std_LOGIC :='0';												-- Flag to start transmitting data on tx pin
+	variable process_out_flag:	std_LOGIC :='0';
+	variable UART_buffer: 		STD_LOGIC_VECTOR (9 DOWNTO 0) :="0000000000";		-- Array to store incoming data
+	variable process_buffer: 	STD_LOGIC_VECTOR (9 DOWNTO 0) :="0000000000";
 	
 	
-	variable data_receiving: STD_LOGIC :='0';												-- Flag to indicate incoming serial data
+	variable data_receiving: 		STD_LOGIC :='0';												-- Flag to indicate incoming serial data
+	variable process_receiving: 	STD_LOGIC :='0';
 	
 	begin
 			if clk' event and clk='1' then  													-- The IF condition is executed during the rising edge of the clock
@@ -85,34 +94,34 @@ begin
 -- A seperate counter is used to send the character stored in array at a rate of 9600 bits/sec.
 						
 						if(tx_flag='1' and counter_2=0) then
-								tx<=UART_buffer(0);
+								process_out<=UART_buffer(0);
 								
 						elsif (tx_flag='1' and counter_2=5208) then
-								tx<=UART_buffer(1);
+								process_out<=UART_buffer(1);
 								
 						elsif (tx_flag='1' and counter_2=10416) then
-								tx<=UART_buffer(2);
+								process_out<=UART_buffer(2);
 								
 						elsif (tx_flag='1' and counter_2=15624) then
-								tx<=UART_buffer(3);
+								process_out<=UART_buffer(3);
 								
 						elsif (tx_flag='1' and counter_2=20832) then
-								tx<=UART_buffer(4);
+								process_out<=UART_buffer(4);
 								
 						elsif (tx_flag='1' and counter_2=26040) then
-								tx<=UART_buffer(5);
+								process_out<=UART_buffer(5);
 								
 						elsif (tx_flag='1' and counter_2=31248) then
-								tx<=UART_buffer(6);
+								process_out<=UART_buffer(6);
 						
 						elsif (tx_flag='1' and counter_2=36456) then
-								tx<=UART_buffer(7);
+								process_out<=UART_buffer(7);
 								
 						elsif (tx_flag='1' and counter_2=41664) then
-								tx<=UART_buffer(8);
+								process_out<=UART_buffer(8);
 								
 						elsif (tx_flag='1' and counter_2=46872) then
-								tx<=UART_buffer(9);
+								process_out<=UART_buffer(9);
 								tx_flag:='0';															-- Initializes counter and flag
 								counter_2:=0;	
 								
@@ -121,6 +130,116 @@ begin
 						if (tx_flag='1') then
 						counter_2:=counter_2+1;														-- Updates counter_2
 						end if;
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						if (process_in='0' and process_counter=0) then										-- Detects Start bit of the message
+								process_receiving:='1';
+								
+-- Receiving UART message on rx	
+-- Counter values are hard coded to receive 9600 bits/sec. Each bit is stored in an array.
+								
+						elsif (process_counter=2604 and process_receiving='1') then
+								process_buffer(0):=process_in;
+						
+						elsif (process_counter=7812 and process_receiving='1') then
+								process_buffer(1):=process_in;
+								
+						elsif (process_counter=13020 and process_receiving='1') then
+								process_buffer(2):=process_in;
+						
+						elsif (process_counter=18228 and process_receiving='1') then
+								process_buffer(3):=process_in;
+								
+						elsif (process_counter=23436 and process_receiving='1') then
+								process_buffer(4):=process_in;
+						
+						elsif (process_counter=28644 and process_receiving='1') then
+								process_buffer(5):=process_in;
+								
+						elsif (process_counter=33852 and process_receiving='1') then
+								process_buffer(6):=process_in;
+								
+						elsif (process_counter=39060 and process_receiving='1') then
+								process_buffer(7):=process_in;
+								
+						elsif (process_counter=44268 and process_receiving='1') then
+								process_buffer(8):=process_in;
+						
+						elsif (process_in='1' and process_receiving='1' and counter=49476) then
+								process_counter:=0;
+								process_receiving:='0';
+								process_out_flag:='1';
+								process_buffer(9):=process_in;
+								
+						end if;
+						
+						
+						if(process_receiving='1') then
+								process_counter:=process_counter+1;
+						end if;
+
+	
+-- Sending UART message on tx	
+-- A seperate counter is used to send the character stored in array at a rate of 9600 bits/sec.
+						
+						if(process_out_flag='1' and process_counter_2=0) then
+								tx<=process_buffer(0);
+								
+						elsif (process_out_flag='1' and process_counter_2=5208) then
+								tx<=process_buffer(1);
+								
+						elsif (process_out_flag='1' and process_counter_2=10416) then
+								tx<=process_buffer(2);
+								
+						elsif (process_out_flag='1' and process_counter_2=15624) then
+								tx<=process_buffer(3);
+								
+						elsif (process_out_flag='1' and process_counter_2=20832) then
+								tx<=process_buffer(4);
+								
+						elsif (process_out_flag='1' and process_counter_2=26040) then
+								tx<=process_buffer(5);
+								
+						elsif (process_out_flag='1' and process_counter_2=31248) then
+								tx<=process_buffer(6);
+						
+						elsif (process_out_flag='1' and process_counter_2=36456) then
+								tx<=process_buffer(7);
+								
+						elsif (process_out_flag='1' and process_counter_2=41664) then
+								tx<=process_buffer(8);
+								
+						elsif (process_out_flag='1' and process_counter_2=46872) then
+								tx<=process_buffer(9);
+								process_out_flag:='0';															-- Initializes counter and flag
+								process_counter_2:=0;	
+								
+						end if;
+						
+						if (process_out_flag='1') then
+						process_counter_2:=process_counter_2+1;														-- Updates counter_2
+						end if;
+						
+						
+						
+						
 						
 				
 			end if;
---
+++ FPGA_Source_Code/output_files/Source.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("C:/Users/Leon/Desktop/FPGA-CoProcessor/FPGA_Source_Code/output_files/") File("Source.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
