d3405946eb5230a0107fb1f4a281fe322d39277b
danielpancake <dsatarov@outlook.com>
2021-11-21 20:21:07 +0300

    New FPGA branch

---
+++ FPGA-CoProc/reg_8.v
@@ -0,0 +1,18 @@
+// 8-bit register
+module reg_8(reset, clk, data, out);
+
+	input reset;
+	input clk;
+	input [7:0] data;
+
+	output [7:0] out;
+	reg [7:0] out;
+
+	always @(posedge clk) begin
+		if (reset)
+			out = 0;
+		else
+			out = data;
+	end
+	
+endmodule
