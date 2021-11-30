37f4228fb9b5949b1fdeb91292c32023e46c2945
Leon-Parepko <l.parepko@innopolis.university>
2021-11-30 06:59:14 +0600

    added 8-bit multiplexor (input format'num1 num2 num3'), returnlast number

--- FPGA_Source_Code/demux_1_to_3_bus_8.v
+++ FPGA_Source_Code/demux_1_to_3_bus_8.v
@@ -1,31 +1,60 @@
 
-module demux_1_to_3_bus_8(in, sel, clk, out1, out2, out3);
+module demux_1_to_3_bus_8(in, clk, i_ready, out1, out2, out3, o_ready, reset);
 
 	input [7:0] in;
-	input [1:0] sel;
 	input clk;
+	input i_ready;
+	input reset;
 	
 	output [7:0] out1;
 	output [7:0] out2;
 	output [7:0] out3;
+	output o_ready;
 	
-	reg [7:0] out1;
-	reg [7:0] out2;
-	reg [7:0] out3;
+	reg [7:0] out1 = 0;
+	reg [7:0] out2 = 0;
+	reg [7:0] out3 = 0;
+	reg o_ready = 0;
+	integer counter = 1; 
+	 
 	
 	always @(posedge clk) begin
-		case (sel)
-			2'b00: begin
+	
+		o_ready <= 1'b0;
+	
+	   if (i_ready == 1)
+      begin
+			counter = counter + 1;
+      end
+		
+		
+		if (reset == 1)
+		begin
+			counter = 1;
+			out1 = 0;
+			out2 = 0;
+			out3 = 0;
+		end
+		
+		
+	
+		case (counter)
+			1: begin
 				out1 = in;
 			end
 			
-			2'b01: begin
+			2: begin
 				out2 = in;
 			end
 				
-			2'b10: begin
+			3: begin
 				out3 = in;
+				o_ready <= 1'b1;
+				counter = 0;
 			end
+			
 		endcase
+		
 	end
+	
 endmodule
